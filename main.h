42bb4d76be12e932d957783ac9da44dbeb3ae65c
Elijovial <omeehelijah345@gmail.com>
2023-06-26 21:16:39 -0700

    Task

--- main.h
+++ main.h
@@ -11,5 +11,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
 char *binar(va_list list);
+char *_intst(int div, int length, int n);
+char *print_d(va_list list);
 
 #endif /* Hannah Sado and Elijah Omeruah*/
--- testing_file/main.c
+++ testing_file/main.c
@@ -40,5 +40,6 @@ int main(void)
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+    _printf("%b\n", 98);
     return (0);
 }
